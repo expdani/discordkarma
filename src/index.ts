e7fa7d8fea84ec792c8ebcf3d6a05d5afb652162
nbennink <nieck.bennink@gmail.com>
2020-02-29 23:03:49 +0100

    Chore: Change Rollup for nodemon and TSC

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,10 @@
-export const blaat = () => {
-    console.log('woei');
-}
+import Discord from "discord.js";
 
-blaat();
+const client = new Discord.Client();
+require("dotenv").config();
+
+client.on("ready", () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.login(process.env.DISCORD_API_KEY);
