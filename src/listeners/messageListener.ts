57470fc049fd299f17645a4594bcde60f4a5e45e
gwesseling <gerardwesseling16@gmail.com>
2020-09-18 17:54:58 +0200

    Refactor: added command resolver instread of switch case

--- src/listeners/messageListener.ts
+++ src/listeners/messageListener.ts
@@ -1,27 +1,23 @@
 import {Message} from "discord.js";
 import {client} from "../";
-import {setupCurrencyCommands} from "../controllers/currency/commands";
 import {calculateResponse} from "../commandHandler";
-import setupMinigameCommands from "../controllers/minigames";
+import commandResolver from "../commandResolver";
 
 /**
  * Listener that listens to messages send in a server
  */
-export default function setupMessageListeners() {
+export default function setupMessageListener() {
     client.on("message", async (message: Message) => {
         const response = await calculateResponse(message);
 
         if (response) {
-            switch (response.command?.command) {
-                case "bal":
-                    setupCurrencyCommands(message);
-                    break;
-                case "trivia":
-                    setupMinigameCommands(message);
-                    break;
-                default:
-                    message.channel.send(response.response);
-                    break;
+            const command = response.command?.command || "";
+            const resolver = commandResolver[command];
+
+            if (resolver) {
+                resolver(message);
+            } else {
+                message.channel.send(response.response);
             }
         }
     });
