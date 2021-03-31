7f0373afca3571a704b5b643ce48092906a61eab
expdani <daniverhaar@hotmail.com>
2021-03-31 18:27:26 +0200

    Permission to run

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 ps -ef | grep discordkarma | grep -v grep | awk '{print $2}' | xargs kill
 git fetch --all
 git reset --hard origin/master
+chmod 777 run.sh
 yarn build
 nohup node /home/discord/discordkarma/build/src/index.js > /home/discord/discordkarma/logs.log 2>&1 &
