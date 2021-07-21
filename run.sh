a4745a2af600b69d62abdc6fc5673d9ca9ee5cab
expdani <daniverhaar@hotmail.com>
2021-07-21 14:11:09 +0200

    Changed run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
-ps -ef | grep discordkarma | grep -v grep | awk '{print $2}' | xargs kill
+ps -ef | grep karma | grep -v grep | awk '{print $2}' | xargs kill
 git fetch --all
 git reset --hard origin/master
 chmod 777 run.sh
 yarn build
-nohup node /home/discord/discordkarma/build/src/index.js > /home/discord/discordkarma/logs.log 2>&1 &
+nohup node /home/discord/karma/build/src/index.js > /home/discord/karma/logs.log 2>&1 &
