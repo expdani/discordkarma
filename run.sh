d9fd1d2ca854a78567c3cfab190010468e254289
expdani <daniverhaar@hotmail.com>
2021-07-24 00:24:25 +0200

    attempt

--- run.sh
+++ run.sh
@@ -1,11 +1,12 @@
-echo $1
+echo $7
 # sudo ps -ef | sudo grep karma | sudo grep -v grep | sudo awk '{print $2}' | sudo xargs kill
-set DISCORD_API_KEY=$2
-set BOT_OWNER_ID=$3
-set DATABASE_HOST=$4
-set DATABASE_USER=$5
-set DATABASE_PASSWORD=$6
-set DATABASE_NAME=$7
-set DATABASE_PORT=$8
+set DISCORD_API_KEY=$1
+set BOT_OWNER_ID=$2
+set DATABASE_HOST=$3
+set DATABASE_USER=$4
+set DATABASE_PASSWORD=$5
+set DATABASE_NAME=$6
+set DATABASE_PORT=$7
+
 nohup node $1/build/src/index.js > /home/discord/logs.log 2>&1 &
 exit
