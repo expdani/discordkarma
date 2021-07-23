c80e34d3cd17ca306340793149b6c8efd3ab12f7
expdani <daniverhaar@hotmail.com>
2021-07-23 21:59:31 +0200

    permission fix

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,10 @@ import setupReactionListeners from "./listeners/reactionListener";
 /**
  * Setup Discord.JS client
  */
+
+console.log(process.env);
+console.log(process);
+
 export const client = new Discord.Client({partials: ["MESSAGE", "CHANNEL", "REACTION"]});
 client.login(env.DISCORD_API_KEY || process.env.DISCORD_API_KEY);
 
