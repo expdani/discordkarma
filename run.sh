6d23b6c8795f6ce1e2184f4d1ea8050e44bdc99d
expdani <daniverhaar@hotmail.com>
2021-07-24 00:36:54 +0200

    attempt

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 echo $1
-# sudo ps -ef | sudo grep karma | sudo grep -v grep | sudo awk '{print $2}' | sudo xargs kill
+sudo ps -ef | sudo grep discord-karma | sudo grep -v grep | sudo awk '{print $2}' | sudo xargs kill
 export DISCORD_API_KEY=$2
 export BOT_OWNER_ID=$3
 export DATABASE_HOST=$4
